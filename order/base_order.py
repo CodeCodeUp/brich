6ca06987fcf0992e3bd678fe3087206b65e4894e
m088607 <m088607@sae.com>
2025-04-07 14:04:20 +0800

    save

--- order/base_order.py
+++ order/base_order.py
@@ -52,10 +52,11 @@ def is_O(num):
             return False
 
 
-def is_order_now():
+def is_order_now(draw_type, pick, strategy_type):
     engine = create_engine('mysql+pymysql://root:202358hjq@116.205.244.106:3306/brich')
-    query = "SELECT count(1) FROM auto_order WHERE nextOrder = 0 OR isFinish = 0"
-    df = pd.read_sql(query, engine)
+    query = ("SELECT count(1) FROM auto_order WHERE  drawType = %s AND pick = %s AND type = %s AND"
+             "(nextOrder = 0 OR isFinish = 0)")
+    df = pd.read_sql(query, engine, params=(draw_type, pick, strategy_type))
     if df.empty:
         return False
     count = df.iloc[0, 0]
@@ -76,16 +77,17 @@ def get_request_id():
     return last_request_id
 
 
-def get_base_data(data_type):
+def get_base_data(data_type, limit):
     engine = create_engine('mysql+pymysql://root:202358hjq@116.205.244.106:3306/brich')
     query = """
         SELECT * 
         FROM base_data 
         WHERE `type` = %(data_type)s 
-        ORDER BY id DESC limit 8
+        ORDER BY id DESC limit %(limit)s
     """
     df = pd.read_sql(query, engine, params={
         'data_type': data_type
+        , 'limit': limit
     })
     # 类型转换...
     return df
@@ -95,27 +97,30 @@ def execute_api_request(data):
     url = 'https://www.ub8.com/ajax/board-game/order'
 
     try:
-        response = requests.post(url, json=data, cookies=cookies)
-        response.raise_for_status()
+        # response = requests.post(url, json=data, cookies=cookies)
+        # response.raise_for_status()
         return True
     except requests.RequestException as e:
         logging.error(f"请求接口时出错: {e}")
         return False
 
 
-def insert_data(request_id, draw_type, draw_number, stake, pick, dice_multiplier, base, draw_total):
+def insert_data(request_id, draw_type, draw_number, stake, pick, dice_multiplier, base, draw_total, strategy_type):
     engine = create_engine('mysql+pymysql://root:202358hjq@116.205.244.106:3306/brich')
     try:
         with engine.begin() as conn:
             # 使用 text 函数将 SQL 字符串包装成可执行对象
             logging.info(f"开始执行插入操作: {request_id}")
             query = text("""
-                            INSERT INTO auto_order (requestId, drawType, drawNumber, stake, pick, base, diceMultiplier, total)
-                            SELECT :request_id, :draw_type, :draw_number, :stake, :pick, :base, :dice_multiplier, :total
+                            INSERT INTO auto_order (requestId, drawType, drawNumber,
+                             stake, pick, base, diceMultiplier, total, type)
+                            SELECT :request_id, :draw_type, :draw_number, 
+                            :stake, :pick, :base, :dice_multiplier, :total, :type
                             FROM DUAL
                             WHERE NOT EXISTS (
                                 SELECT 1 FROM auto_order
-                                WHERE drawType = :draw_type AND drawNumber = :draw_number AND pick = :pick
+                                WHERE drawType = :draw_type AND drawNumber = :draw_number 
+                                AND pick = :pick AND type = :type
                             )
                         """)
 
@@ -127,7 +132,8 @@ def insert_data(request_id, draw_type, draw_number, stake, pick, dice_multiplier
                 'pick': pick,
                 'base': base,
                 'dice_multiplier': dice_multiplier,
-                'total': draw_total
+                'total': draw_total,
+                'type': strategy_type
             })
             logging.info(f"插入成功: {request_id}")
 
@@ -144,7 +150,12 @@ def process_un_orders():
 
             for _, row in df.iterrows():
                 total = row["total"]
-                bet = int(row["stake"]) * fib_sequence[int(row["base"])]
+                strategy_type = row["type"]
+                bet = 0
+                if strategy_type == 1:
+                    bet = int(row["stake"]) * fib_sequence[int(row["base"])]
+                if strategy_type == 2:
+                    bet = int(row["stake"]) * int(row["base"])
                 data = {
                     "requestId": row["requestId"],
                     "drawType": row["drawType"],
@@ -202,6 +213,7 @@ def process_un_finish():
                 draw_stake = row["stake"]
                 draw_base = int(row["base"])
                 draw_total = row["total"]
+                strategy_type = row["type"]
                 df_last = pd.read_sql(query_last, engine, params=(6, draw_number))
                 if df_last.empty:
                     continue
@@ -226,10 +238,16 @@ def process_un_finish():
                     return
                 logging.info(f"draw_pick {draw_pick}，value: {value}")
                 if value == draw_pick:
-                    draw_total = int(draw_total) + (int(draw_stake) * fib_sequence[int(draw_base)] * 2)
-                    draw_base = max(0, draw_base - 2)
+                    if strategy_type == 1:
+                        draw_total = int(draw_total) + (int(draw_stake) * fib_sequence[int(draw_base)] * 2)
+                        draw_base = max(0, draw_base - 2)
+                    if strategy_type == 2:
+                        draw_total = int(draw_total) + (int(draw_stake) * int(draw_base) * 2)
                 else:
-                    draw_base = draw_base + 1
+                    if strategy_type == 1:
+                        draw_base = draw_base + 1
+                    if strategy_type == 2:
+                        draw_base = draw_base * 2
                 with engine.begin() as conn:  # 自动提交事务
                     update_query = text("""
                         UPDATE auto_order 
@@ -240,16 +258,21 @@ def process_un_finish():
                         WHERE id = :id
                     """)
                     conn.execute(update_query, {"id": row['id'], "total": draw_total})
-                if int(draw_total) >= (2000 + int(row["stake"]) * 3):
-                    logging.info("完成此次下单")
-                    continue
+                if strategy_type == 1:
+                    if int(draw_total) >= (2000 + int(row["stake"]) * 3):
+                        logging.info("类型1，完成此次下单")
+                        continue
+                if strategy_type == 2:
+                    if int(draw_total) >= (2000 + int(row["stake"])) or int(draw_base) > 4:
+                        logging.info("类型2，完成此次下单")
+                        continue
                 num = str(int(draw_number[8:]) + 1).zfill(4)
                 # 数据库读取最新的requestId
                 df_request_id = pd.read_sql(query_request_id, engine)
                 # 16进制+1
                 request_id = str(hex(int(df_request_id['requestId'].tolist()[0], 16) + 1))[2:]
                 insert_data(request_id, draw_type, f"{draw_number[:8]}-{num}",
-                            draw_stake, draw_pick, 1, draw_base, draw_total)
+                            draw_stake, draw_pick, 1, draw_base, draw_total, strategy_type)
                 logging.info(f"插入下一条数据成功, requestId: {request_id}, drawNumber: {draw_number[:8]}-{num}")
         except Exception as e:
             logging.error(f"process_un_finish: {e}")
@@ -258,12 +281,10 @@ def process_un_finish():
 
 def process_do_order():
     while True:
-        if is_order_now():
-            logging.info("当前有订单正在进行，跳过本次插入")
-            return
-        df = get_base_data(6)
+        df = get_base_data(6, 8)
         if df.empty:
-            return
+            time.sleep(20)
+            continue
         data = df['number_four'].tolist()
         # 如果全部is_SB_B或is_SB_S且没有正在进行的,插入auto_order数据，pick相反
         if all(is_SB_B(int(num)) for num in data):
@@ -273,6 +294,11 @@ def process_do_order():
             pick = 'BIG'
             logging.info(f"当前数据全部为小: {data}")
         else:
+            time.sleep(20)
+            continue
+        if is_order_now('F1TB', pick, 1):
+            logging.info("当前有订单正在进行，跳过本次插入")
+            time.sleep(20)
             continue
         # 获取最新的requestId
         request_id = get_request_id()
@@ -285,8 +311,61 @@ def process_do_order():
         base = 0
         dice_multiplier = 1
         draw_total = 2000
-        insert_data(request_id, draw_type, draw_number, stake, pick, dice_multiplier, base, draw_total)
-        time.sleep(5)
+        insert_data(request_id, draw_type, draw_number, stake, pick, dice_multiplier, base, draw_total, 1)
+        time.sleep(30)
+
+
+def process_do_order_two():
+    while True:
+        length = 8
+        df = get_base_data(6, length)
+        # 切分为前七个和最后一个
+        if df.empty:
+            time.sleep(20)
+            continue
+        if len(df) < length:
+            time.sleep(20)
+            continue
+        data = df['number_four'].tolist()
+        before = length - 1
+        # 前七个
+        data_before = data[:before]
+        # 后一个
+        data_1 = data[-1]
+        # 如果前七个和最后一个不同且没有正在进行的,插入auto_order数据，pick相反
+        if all(is_SB_B(int(num)) for num in data_before) and not is_SB_B(int(data_1)):
+            pick = 'SMALL'
+            logging.info(f"当前数据全部为大: {data}")
+        elif all(is_SB_S(int(num)) for num in data_before) and not is_SB_S(int(data_1)):
+            pick = 'BIG'
+            logging.info(f"当前数据全部为小: {data}")
+        elif all(is_D(int(num)) for num in data_before) and not is_D(int(data_1)):
+            pick = 'ODD'
+            logging.info(f"当前数据全部为偶: {data}")
+        elif all(is_O(int(num)) for num in data_before) and not is_O(int(data_1)):
+            pick = 'EVEN'
+            logging.info(f"当前数据全部为奇: {data}")
+        else:
+            time.sleep(20)
+            continue
+        if is_order_now('F1TB', pick, 2):
+            logging.info("当前有订单正在进行，跳过本次插入")
+            time.sleep(20)
+            continue
+        # 获取最新的requestId
+        request_id = get_request_id()
+        draw_type = 'F1TB'
+        # draw_number 为最后一个nid+1
+        number = df['nid'].tolist()[0]
+        num = str(int(number[8:]) + 1).zfill(4)
+        draw_number = f"{number[:8]}-{num}"
+        stake = 10
+        base = 1
+        dice_multiplier = 1
+        draw_total = 2000
+        insert_data(request_id, draw_type, draw_number, stake,
+                    pick, dice_multiplier, base, draw_total, 2)
+        time.sleep(30)
 
 
 def keep_alive():
@@ -320,9 +399,11 @@ if __name__ == "__main__":
     thread2 = threading.Thread(target=process_un_finish)
     thread3 = threading.Thread(target=process_do_order)
     thread4 = threading.Thread(target=keep_alive)
+    thread5 = threading.Thread(target=process_do_order_two)
     thread1.start()
     thread2.start()
     thread3.start()
     thread4.start()
+    thread5.start()
 
 
